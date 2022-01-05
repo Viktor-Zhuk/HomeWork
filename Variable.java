0d022a3b6776fbcde0db3f5408e19e0299b683a5
Жук Виктор Валентинович <C:\Users\viktor\AppData\Roaming\The Bat!>
2022-01-05 15:47:48 +0300

    Домашнее задание.

---
+++ Variable.java
@@ -0,0 +1,12 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte b = 10;
+		short s = 500;
+		int i = 1000;
+		long l = 1000000L;
+		float f = 3.144F;
+		double d = 10.005;
+		char ch = 'a';
+		boolean boo = true;
+	}
+}
