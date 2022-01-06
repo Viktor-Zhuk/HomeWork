15ffe34804fe3bbd438fbc1419a3f9d855a254af
unknown <C:\Users\Viktor\AppData\Roaming\The Bat!>
2022-01-06 22:18:33 +0300

    Home work about cycle.

---
+++ Lesson_1/Cycle.java
@@ -0,0 +1,27 @@
+public class Cycle {
+    
+    public static void main(String[] args) {
+
+        for(int i = 0; i<=20; i++) {
+            System.out.println(i);
+        }
+
+        int i = 6;
+        while (i >= -6) {
+            System.out.println(i);
+            i = i - 2;
+        }
+
+        i = 10;
+        int sum = 0;
+        do {
+            if(i % 2 != 0) {
+                sum = sum + i;
+            }
+            i++;
+        } while(i <= 20);
+
+        System.out.println("Сумма нечетных чисел от 10 до 20 равна: " + sum);
+
+    }
+}
