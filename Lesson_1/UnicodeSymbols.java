2d9c17ef642791bb2aa53ebf9c7ad2ad4e749e0b
Жук Виктор Валентинович <C:\Users\viktor\AppData\Roaming\The Bat!>
2022-01-10 13:52:18 +0300

    HW 10.01

--- Lesson_1/UnicodeSymbols.java
+++ Lesson_1/UnicodeSymbols.java
@@ -1,8 +1,8 @@
 public class UnicodeSymbols {
     
     public static void main(String[] args) {
-        for (int i = 33; i <= 126; i++) {
-            System.out.println((char)i);
+        for (char i = 33; i <= 126; i++) {
+            System.out.println(i);
         }
     }
 }
--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -2,20 +2,20 @@ public class Variable {
     
     public static void main(String[] args) {
         byte numberOfCores = 6;
-        short powerUnit = 65;
+        short thermalPower = 65;
         int graphicsFrequency = 350;
         long diskSpace = 1000000L;
         float clockFrequency = 3.50F;
         double operationalMemory = 16.0;
         char processorIndex = 'i';
         boolean isСamera = true;
-        System.out.println("numberByte: " + numberOfCores);
-        System.out.println("numberShort: " + powerUnit);
-        System.out.println("numberInt: " + graphicsFrequency);
-        System.out.println("numberLong: " + diskSpace);
-        System.out.println("numberFloat: " + clockFrequency);
-        System.out.println("numberDouble: " + operationalMemory);
-        System.out.println("letter: " + processorIndex);
-        System.out.println("isTrue: " + isСamera);
+        System.out.println("Количество ядер: " + numberOfCores);
+        System.out.println("Тепловая мощность: " + thermalPower);
+        System.out.println("Частота графического процессора: " + graphicsFrequency);
+        System.out.println("Объем дискового пространства: " + diskSpace);
+        System.out.println("Тактовая частота: " + clockFrequency);
+        System.out.println("Объем оперативной памяти: " + operationalMemory);
+        System.out.println("Индекс маркировки процессора: " + processorIndex);
+        System.out.println("Наличие видеокамеры: " + isСamera);
     }
 }
