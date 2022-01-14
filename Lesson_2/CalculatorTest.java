9c9fe0f96b9debfd7025a0c44eadc3e716eb3dc9
Жук Виктор Валентинович <C:\Users\viktor\AppData\Roaming\The Bat!>
2022-01-14 15:52:51 +0300

    HW 14.01

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,14 +4,32 @@ public class CalculatorTest {
     
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите первое число: ");
-        int firstNumber = sc.nextInt();
-        System.out.println("Введите знак математической операции: ");
-        char mathOperation = sc.next().charAt(0);
-        System.out.println("Введите второе число: ");
-        int secondNumber = sc.nextInt();
+        boolean helper = true;
 
-        Calculator calculator = new Calculator();
-        calculator.calculate(firstNumber, secondNumber, mathOperation);
+        while (helper) {
+            System.out.println("Введите первое число: ");
+            int firstNumber = sc.nextInt();
+            System.out.println("Введите знак математической операции: ");
+            char mathOperation = sc.next().charAt(0);
+            System.out.println("Введите второе число: ");
+            int secondNumber = sc.nextInt();
+
+            Calculator calculator = new Calculator();
+            calculator.calculate(firstNumber, secondNumber, mathOperation);
+            
+            boolean helper2 = true;
+            while (helper2) {
+                System.out.println("Хотите продолжить вычисления? [yes/no]");
+                String answer = sc.next();
+                if (answer.equals("no")) {
+                    helper = false;
+                    helper2 = false;
+                } else if (answer.equals("yes")) {
+                    helper2 = false;
+                } else {
+                    System.out.println("Введите корректный ответ.");
+                }
+            }            
+        }
     }
 }
