4569340632f97372d9bd63755540867760c24d59
Viktor <v.zhuk@mahorka.com>
2022-02-06 21:44:39 +0300

    HW 06.02

--- src/com/startjava/lesson_2_3_4/game/GuessNumber.java
+++ src/com/startjava/lesson_2_3_4/game/GuessNumber.java
@@ -1,48 +1,59 @@
 package com.startjava.lesson_2_3_4.game;
 
-import javafx.print.PageLayout;
-
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 
 public class GuessNumber {
 
-    private Player playerOne;
-    private Player playerTwo;
+    private Player[] players = new Player[2];
 
     public GuessNumber(Player playerOne, Player playerTwo) {
-        this.playerOne = playerOne;
-        this.playerTwo = playerTwo;
+        players[0] = playerOne;
+        players[1] = playerTwo;
     }  
 
     public void start() {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
         int number = random.nextInt(100) + 1;
-        Player[] playersInGame = {playerOne, playerTwo};
-        int i = 2;
-        while (playersInGame[1].getCountAttempts() < 10) {
-            int j = i % 2;
-            System.out.print(playersInGame[j] + " ваш вариант ответа: ");
-            int numberVariant = sc.nextInt();
-            playersInGame[j].addNumber(numberVariant);
-            if (numberVariant > number) {
-                System.out.println("Данное число больше того, что загадал компьютер");
-            } else if (numberVariant < number) {
-                System.out.println("Данное число меньше того, что загадал компьютер");
-            } else {
-                System.out.println("Поздравляем! " + playersInGame[j] + " вы угадали.");
-                System.out.println("Игрок " + playersInGame[j] + " угадал число " + numberVariant + " с " +
-                        playersInGame[j].getCountAttempts() + " попытки");
+        boolean isPlay = true;
+        while (isPlay) {
+            for (int i = 0; i < players.length; i++) {
+                System.out.print(players[i] + " ваш вариант ответа: ");
+                int playerAnswer = sc.nextInt();
+                players[i].addNumber(playerAnswer);
+                if (checkNumber(players[i], playerAnswer, number)) {
+                    isPlay = false;
+                    break;
+                }
+                if (players[i].getCountAttempts() == 10) {
+                    System.out.println("У " + players[i] + " закончились попытки");
+                }
+            }
+            if (players[1].getCountAttempts() == 10) {
                 break;
             }
-            if (playersInGame[j].getCountAttempts() == 10) {
-                System.out.println("У " + playersInGame[j] + " закончились попытки");
-            }
-            i++;
         }
-        for (i = 0; i < playersInGame.length; i++) {
+        printValues(players);
+    }
+
+    public boolean checkNumber(Player player, int playerAnswer, int number) {
+        boolean isСorrectАnswer = false;
+        if (playerAnswer > number) {
+            System.out.println("Данное число больше того, что загадал компьютер");
+        } else if (playerAnswer < number) {
+            System.out.println("Данное число меньше того, что загадал компьютер");
+        } else {
+            System.out.println("Поздравляем! " + player + " вы угадали.");
+            System.out.println("Игрок " + player + " угадал число " + playerAnswer + " с " +
+                    player.getCountAttempts() + " попытки");
+            isСorrectАnswer = true;
+        }
+        return isСorrectАnswer;
+    }
+
+    public void printValues(Player[] playersInGame) {
+        for (int i = 0; i < playersInGame.length; i++) {
             int[] enteredNumbers = playersInGame[i].getNumbers();
             System.out.print(playersInGame[i] + " ");
             for (int num : enteredNumbers) {
